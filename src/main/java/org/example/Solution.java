e28f1b28b4366b19875936e5f0eb9df78f5ffa42
Alvin <alvinngg9@gmail.com>
2024-08-19 18:33:56 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,27 +1,36 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> {
-            return Math.abs(target - a) - Math.abs(target - b);
-        });
+        //create a priority queue that puts the sum of three integers that is closest
+        //to 0 in the front
+        PriorityQueue<Integer> pq =
+                new PriorityQueue<>(Comparator.comparingInt(a -> Math.abs(a - target)));
+        //sort the given array
         Arrays.sort(nums);
+        //iterate through the given array
         for (int i = 0; i < nums.length - 2; i++) {
             int j = i + 1;
             int k = nums.length - 1;
-            while(j < k) {
-                if (nums[i] + nums[j] + nums[k] - target <= 0) {
-                    pq.offer(nums[i] + nums[j] + nums[k]);
+            //check for all sums while index j is less than index k
+            while (j < k) {
+                //add the current sum to the priority queue
+                int sum = nums[i] + nums[j] + nums[k];
+                pq.offer(sum);
+                //move the two pointers based on if the sum subtracts the target is
+                //less than, or bigger than 0
+                if (sum - target < 0) {
                     j++;
                 } else {
-                    pq.offer(nums[i] + nums[j] + nums[k]);
                     k--;
                 }
             }
         }
+        //return the top of the queue
         return pq.remove();
     }
 }
